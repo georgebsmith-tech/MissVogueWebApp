47281a6e97e0c95ad666b59e7b46ebe8ccf29a97
George, B. Smith <georgebsmith.tech@gmail.com>
2020-08-04 02:13:16 -0700

    nothing

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -2,7 +2,7 @@ function checkAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
         return next()
     }
-    res.redirect("/admins/login")
+    res.redirect("/admin/login")
 }
 
 
