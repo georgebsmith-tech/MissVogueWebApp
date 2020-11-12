a0d5823bc06b5dbf63df246a792044a2a217ac1a
George, B. Smith <georgebsmith.tech@gmail.com>
2020-11-12 02:07:09 -0800

    added cors

--- routes/api/contestantsRoutes.js
+++ routes/api/contestantsRoutes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router()
 const ContestantsModel = require("../../models/contestantsModel");
 const adminAuthenticated = require("../../middleware/admin-auth-check")
+const cors = require("cors")
 
 router.get("/", async (req, res) => {
     const data = await ContestantsModel.find()
@@ -8,7 +9,7 @@ router.get("/", async (req, res) => {
     res.status(200).json({data})
 })
 
-router.post("/",adminAuthenticated, async (req, res) => {
+router.post("/",adminAuthenticated,cors(), async (req, res) => {
     const contestant = new ContestantsModel(req.body)
     const data = await contestant.save()
     res.status(200).json({
